df22b90b03d69856feeb2f8c8ce5c4d1b3b086d0
Web-Dev <dragonemperor9868@gmail.com>
2024-08-06 16:47:53 +0530

    Delete products from cart when clicking delete

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [{
+export let cart = [{
   id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   quantity:2
 },{
@@ -24,4 +24,14 @@ export function addToCart(productId) {
         quantity: selectedQuantity
       });
     }
+  }
+
+  export function removeFromCart(productId){
+    const newCart = [];
+    cart.forEach((cartItem)=>{
+      if(cartItem.id!==productId){
+        newCart.push(cartItem);
+      }
+    });
+    cart  = newCart;
   }
