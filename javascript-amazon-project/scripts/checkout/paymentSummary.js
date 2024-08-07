04f4fe692e0722ec619397f3d1ac55b631be9651
Web-Dev <dragonemperor9868@gmail.com>
2024-08-08 01:00:31 +0530

    Updating Item quantity in payment summary

--- javascript-amazon-project/scripts/checkout/paymentSummary.js
+++ javascript-amazon-project/scripts/checkout/paymentSummary.js
@@ -1,4 +1,4 @@
-import {cart} from '../../data/cart.js';
+import {cart,totalCartQuantity} from '../../data/cart.js';
 import {getProduct} from '../../data/products.js';
 import { getDeliveryOption } from '../../data/deliveryOptions.js';
 import { formatCurrency } from '../utils/money.js';
@@ -30,7 +30,7 @@ export function renderPaymentSummary(){
           </div>
 
           <div class="payment-summary-row">
-            <div>Items (3):</div>
+            <div>Items (${totalCartQuantity()}):</div>
             <div class="payment-summary-money">$${formatCurrency(productPriceCents)}</div>
           </div>
 
