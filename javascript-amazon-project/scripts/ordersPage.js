ebc2fe9032be4df5d882f3c2fdb00a5761f0732e
Web-Dev <dragonemperor9868@gmail.com>
2024-08-24 23:15:05 +0530

    Unique URL for each product for tracking

--- javascript-amazon-project/scripts/ordersPage.js
+++ javascript-amazon-project/scripts/ordersPage.js
@@ -89,7 +89,7 @@ function OrderDetailsgrid(){
             </div>
 
             <div class="product-actions">
-              <a href="tracking.html">
+              <a href="tracking.html?orderId=${order.id}&productId=${matchingProduct.id}">
                 <button class="track-package-button button-secondary">
                   Track package
                 </button>
