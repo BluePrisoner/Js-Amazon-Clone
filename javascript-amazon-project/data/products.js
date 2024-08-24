f7d19d556bb7f7e7cf832127cdb57c2ec1d7f4a0
Web-Dev <dragonemperor9868@gmail.com>
2024-08-24 15:10:01 +0530

    Convert code to promises

--- javascript-amazon-project/data/products.js
+++ javascript-amazon-project/data/products.js
@@ -87,7 +87,7 @@ export function loadProducts(renderProductsGrid){
       return new Products(productDetails);
     });
 
-    console.log(products);
+    
     renderProductsGrid();
   });
 
--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -3,7 +3,12 @@ import {renderPaymentSummary} from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
 // import '../data/backend-practice.js';
 
-loadProducts(()=>{
+new Promise((resolve)=>{
+   
+    loadProducts(()=>{
+       resolve();
+    });
+}).then(()=>{
     renderOrderSummary();
     renderPaymentSummary();
 });
